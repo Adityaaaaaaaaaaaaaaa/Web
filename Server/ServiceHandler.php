07eaa89fd54d06f98c1e043f52b9adea8136cd52
Aditya <171126285+Adityaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-10-02 18:34:33 +0400

    Denied access via url

--- Server/ServiceHandler.php
+++ Server/ServiceHandler.php
@@ -1,6 +1,27 @@
 <?php
 	require_once '../Service/Service.php'; 
 
+	if (!isset($_SESSION['adminUname'])) {
+		echo ' <!-- jQuery -->
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+			<!-- XSAlert CSS -->
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/frankeno/xsalert@main/src/themes/light-theme.css">
+			<!-- XAlert core JS -->
+			<script src="https://cdn.jsdelivr.net/gh/frankeno/xsalert@main/src/xsalert.js"></script>
+			<script>
+			XSAlert({
+				title: "Sneaking in?",
+				message: "You know what to do",
+				icon: \'error\',
+				hideCancelButton: true,
+				closeOnOutsideClick: false,
+				hideOkButton: true,
+				closeWithESC: false,
+				footer: \'<a href="../home.php">Home</a>\'
+			})
+			</script>';
+	}
+
 	try {
 		$service = new CombinedService();
 
